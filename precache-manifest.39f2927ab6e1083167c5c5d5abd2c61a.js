2a6bb68b33622de8346ba1e44b42387cb2defcb6
M9D9V <m9d9v9@hotmail.com>
2020-10-19 08:28:31 -0500

    Updates

---
+++ precache-manifest.39f2927ab6e1083167c5c5d5abd2c61a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e64d6a45ca90f512aaed29103492f4",
+    "url": "/Examen1/index.html"
+  },
+  {
+    "revision": "9d9dabe3275ec5f3eab1",
+    "url": "/Examen1/static/css/2.f4efcd0f.chunk.css"
+  },
+  {
+    "revision": "2e51237772c6b45d592d",
+    "url": "/Examen1/static/css/main.ce4ca2af.chunk.css"
+  },
+  {
+    "revision": "9d9dabe3275ec5f3eab1",
+    "url": "/Examen1/static/js/2.4958df84.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Examen1/static/js/2.4958df84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e51237772c6b45d592d",
+    "url": "/Examen1/static/js/main.78f7da84.chunk.js"
+  },
+  {
+    "revision": "17e588b66d9787d4e329",
+    "url": "/Examen1/static/js/runtime-main.86618bf8.js"
+  }
+]);
